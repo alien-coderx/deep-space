0d6b5fd24eccd17e59c6b2faeb617b42910f5c11
roger <luojie@vanelink.net>
2026-09-22 15:19:11 +0800

    恢复深空动画效果：星空+渐变+银河漂移+外星人/飞船/太阳系，移除光环

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import StarField from './components/StarField.jsx';
-import RingVisual from './components/RingVisual.jsx';
+import React from 'react';
+import DeepSpaceBackground from './components/DeepSpaceBackground.jsx';
 
 export default function App() {
   return (
     <div className="app-root">
-      <StarField />
-      <div className="deep-space-gradient" />
-      <div className="milky-way" />
-      <div className="ring-wrapper">
-        <RingVisual />
-      </div>
+      <DeepSpaceBackground />
     </div>
   );
 }
